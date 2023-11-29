ada643c146db4f91a8252f313a7a8ed095330cea
Shoffen <61907814+Shoffen@users.noreply.github.com>
2023-11-29 16:21:37 +0300

    update
    Movement

--- Dragon Hunters/Assets/Scripts/PlayerController.cs
+++ Dragon Hunters/Assets/Scripts/PlayerController.cs
@@ -7,27 +7,16 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody myRigidbody;
     public Animator animator;
-    private PlayerInput playerInput;
+    private InputManager playerInput;
 
     public float walkingSpeed;
-    private Vector3 walkingChange;
-    private Vector3 forward, right;
-    private Vector3 heading;
-
     public float rotationSpeed;
-    private float rotationAngle;
-    private Quaternion targetRotation;
+    private Vector3 walkingChange;
 
     private void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
-        playerInput = GetComponent<PlayerInput>();
-
-        forward = Camera.main.transform.forward;
-        forward.y = 0;
-        forward = Vector3.Normalize(forward);
-        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
-
+        playerInput = GetComponent<InputManager>();
     }
 
     private void Update()
@@ -37,22 +26,18 @@ public class PlayerController : MonoBehaviour
 
     private void GetInput()
     {
-        walkingChange = Vector3.zero;
+        //walkingChange = Vector3.zero;
 
-        if (this.playerInput.ListenForHeldDown(PlayerInput.PLAYER_ACTION.GO_LEFT))
-        {
-            walkingChange = new Vector3(-1.0f, 0.0f, 0.0f);
-        }
 
-        if (this.playerInput.ListenForHeldDown(PlayerInput.PLAYER_ACTION.GO_RIGHT))
-        {
-            walkingChange = new Vector3(1.0f, 0.0f, 0.0f);
-        }
 
-        if (this.playerInput.ListenForClick(PlayerInput.PLAYER_ACTION.SHOOT))
+        walkingChange = playerInput.GetAxis(InputManager.AXIS.MOVE);
+
+        
+        //Debug.Log(walkingChange);
+        /*if (this.playerInput.ListenForClick(InputManager.PLAYER_ACTION.SHOOT))
         {
             // Handle shooting logic here
-        }
+        }*/
     }
 
     private void FixedUpdate()
@@ -75,16 +60,28 @@ public class PlayerController : MonoBehaviour
         }
 
         walkingChange = Vector3.ClampMagnitude(walkingChange, 1f);
-        Vector3 rightMovement = right * walkingSpeed * Time.fixedDeltaTime * walkingChange.x;
+        Vector3 movement = walkingChange * walkingSpeed * Time.fixedDeltaTime;
 
-        // Only consider X-axis component for heading
-        heading = Vector3.Normalize(new Vector3(rightMovement.x, 0.0f, 0.0f));
-
-        myRigidbody.position += rightMovement;
+        // Add movement to the current position
+        myRigidbody.MovePosition(myRigidbody.position + movement);
     }
 
-
     private void UpdateRotation()
+    {
+
+        float targetRotationY = walkingChange.x * 90f;
+        if (targetRotationY != 0)
+        {
+            //Debug.Log(targetRotationY);
+            Quaternion targetRotation = Quaternion.Euler(0f, targetRotationY, 0f);
+         // transform.rotation = targetRotation;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        //this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+
+    }
+    /*private void UpdateRotation()
     {
         float rotationInput = walkingChange.x;
 
@@ -97,5 +94,28 @@ public class PlayerController : MonoBehaviour
             targetRotation = Quaternion.Euler(0, rotationAngle, 0);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
         }
-    }
+    }*/
+    /*private void UpdateMovement()
+    {
+        if (walkingChange != Vector3.zero)
+        {
+            animator.SetBool("idle", false);
+            animator.SetBool("walking", true);
+        }
+        else
+        {
+            animator.SetBool("walking", false);
+            animator.SetBool("idle", true);
+        }
+
+        walkingChange = Vector3.ClampMagnitude(walkingChange, 1f);
+        Vector3 rightMovement = right * walkingSpeed * Time.fixedDeltaTime * walkingChange.x;
+
+        // Only consider X-axis component for heading
+        heading = Vector3.Normalize(new Vector3(rightMovement.x, 0.0f, 0.0f));
+
+        myRigidbody.position += rightMovement;
+    }*/
+
+
 }
