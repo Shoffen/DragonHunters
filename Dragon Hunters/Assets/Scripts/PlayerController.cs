125ce445083adfd448284a85b77ee64a33be0a8b
Martynas Cibulis <74449709+onionas1@users.noreply.github.com>
2023-12-03 15:27:49 +0200

    ik update

--- Dragon Hunters/Assets/Scripts/PlayerController.cs
+++ Dragon Hunters/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     private const float ACCELERATION = 12f;
+    private const float AIMING_Y_OFFSET_TARGET = 80f;
+    private const float AIM_PREPARE_DURATION = 4f; // second / n
+    private const float AIM_WEIGHT = 0.5f;
+    private const float IDLE_WEIGHT = 0.1f;
 
     [SerializeField] private Animator animator;
+    [SerializeField] private AnimationCurve aimPrepareCurve;
+    [SerializeField] private MultiAimConstraint[] spineBones;
 
     private Rigidbody myRigidbody;
     private InputManager playerInput;
@@ -22,12 +29,20 @@ public class PlayerController : MonoBehaviour
     public Transform targetTransform;
     public LayerMask mouseAimMask;
 
-    private Transform rigTransform;
-    private Transform aimingBodyTransform;
-    private Transform aimingHandTransform;
-    private Transform aimingHeadTransform;
-    private Transform notAimingBodyTransform;
-    private Transform notAimingHandTransform;
+    private float targetAimDelta;
+    private float weightStart;
+    private float weightTarget;
+    private float startAimDelta;
+    private float aimDeltaTime;
+    private bool isAimStateChangeing;
+    private AIMING_STATE aimingState;
+
+    private enum AIMING_STATE
+    {
+        IDLE,
+        AIMING
+    }
+
     private bool NeedBackwards
     {
         get
@@ -40,21 +55,73 @@ public class PlayerController : MonoBehaviour
     {
         myRigidbody = GetComponent<Rigidbody>();
         playerInput = GetComponent<InputManager>();
+        SetAimingState(AIMING_STATE.IDLE, true);
         mainCamera = Camera.main;
-        rigTransform = transform.Find("Rig 1");
-        Debug.Log(rigTransform.name);
-
-        aimingBodyTransform     = rigTransform.Find("AimingBody");
-        aimingHandTransform     = rigTransform.Find("AimingHand");
-
-        notAimingBodyTransform  = rigTransform.Find("NotAimingBody");
-        notAimingHandTransform  = rigTransform.Find("NotAimingHand");
-        aimingHeadTransform     = rigTransform.Find("AimingHead");
     }
 
     private void Update()
     {
         GetInput();
+    }
+
+    private void SetAimingState(AIMING_STATE _state = AIMING_STATE.IDLE, bool _isInstant = false)
+    {
+        aimingState = _state;
+        isAimStateChangeing = true;
+        if (aimingState == AIMING_STATE.IDLE)
+        {
+            startAimDelta = 1;
+            targetAimDelta = 0;
+            weightStart = AIM_WEIGHT;
+            weightTarget = IDLE_WEIGHT;
+            if (_isInstant)
+            {
+                foreach (MultiAimConstraint constraint in spineBones)
+                {
+                    constraint.data.offset = new Vector3(0, 0, 0);
+                    constraint.weight = IDLE_WEIGHT;
+                }
+                animator.SetLayerWeight(1, targetAimDelta);
+                isAimStateChangeing = false;
+            }
+        }
+        else
+        {
+            weightStart = IDLE_WEIGHT;
+            weightTarget = AIM_WEIGHT;
+            startAimDelta = 0;
+            targetAimDelta = 1;
+            if (_isInstant)
+            {
+                animator.SetLayerWeight(1, targetAimDelta);
+                foreach (MultiAimConstraint constraint in spineBones)
+                {
+                    constraint.data.offset = new Vector3(0, AIMING_Y_OFFSET_TARGET, 0);
+                    constraint.weight = AIM_WEIGHT;
+                }
+                isAimStateChangeing = false;
+            }
+        }
+    }
+
+    private void UpdateAimingState()
+    {
+        if (isAimStateChangeing)
+        {
+            aimDeltaTime = Mathf.Clamp01(aimDeltaTime + Time.deltaTime * AIM_PREPARE_DURATION);
+            animator.SetLayerWeight(1, Mathf.Lerp(startAimDelta, targetAimDelta, aimPrepareCurve.Evaluate(aimDeltaTime)));
+            float _yAxis = Mathf.Lerp(startAimDelta, targetAimDelta * AIMING_Y_OFFSET_TARGET, aimPrepareCurve.Evaluate(aimDeltaTime));
+            foreach (MultiAimConstraint constraint in spineBones)
+            {
+                constraint.data.offset = new Vector3(0, _yAxis, 0);
+                constraint.weight = Mathf.Lerp(weightStart, weightTarget, aimPrepareCurve.Evaluate(aimDeltaTime));
+            }
+            if (aimDeltaTime == 1)
+            {
+                isAimStateChangeing = false;
+                aimDeltaTime = 0;
+            }
+        }
     }
 
     private void GetInput()
@@ -64,90 +131,32 @@ public class PlayerController : MonoBehaviour
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, mouseAimMask))
         {
             targetTransform.position = hit.point;
-           
+        }
+        walkingChange = playerInput.GetAxis(InputManager.AXIS.MOVE);
+        if (isAimStateChangeing)
+        {
+            return;
         }
         if (playerInput.ListenForClick(InputManager.PLAYER_ACTION.AIMING))
         {
-            /*if (animator.GetLayerWeight(1) == 1)
+            if(aimingState == AIMING_STATE.IDLE)
             {
-                //animator.SetLayerWeight(1, Mathf.Lerp(1, 0, Time.deltaTime * 10));
-                animator.SetLayerWeight(1, 0);
-                aimingBodyTransform.gameObject.SetActive(false);
-                aimingHandTransform.gameObject.SetActive(false);
-                aimingHeadTransform.gameObject.SetActive(false);
-
-
-                notAimingBodyTransform.gameObject.SetActive(true);
-                notAimingHandTransform.gameObject.SetActive(true);
+                SetAimingState(AIMING_STATE.AIMING);
             }
             else
             {
-                //animator.SetLayerWeight(1, Mathf.Lerp(0, 1, Time.deltaTime * 10));
-                animator.SetLayerWeight(1, 1);
-                aimingBodyTransform.gameObject.SetActive(true);
-                aimingHandTransform.gameObject.SetActive(true);
-                aimingHeadTransform.gameObject.SetActive(true);
-
-                notAimingBodyTransform.gameObject.SetActive(false);
-                notAimingHandTransform.gameObject.SetActive(false);
-            }*/
-            if (animator.GetBool("Aiming"))
-            {
-                animator.SetBool("Aiming", false);
-                Debug.Log("NESITAIKOM");
-                aimingBodyTransform.gameObject.SetActive(false);
-                aimingHandTransform.gameObject.SetActive(false);
-                aimingHeadTransform.gameObject.SetActive(false);
-
-                notAimingBodyTransform.gameObject.SetActive(true);
-                notAimingHandTransform.gameObject.SetActive(true);
+                SetAimingState(AIMING_STATE.IDLE);
             }
-            else
-            {
-                animator.SetBool("Aiming", true);
-                Debug.Log("TAIKOMES");
-                aimingBodyTransform.gameObject.SetActive(true);
-                aimingHandTransform.gameObject.SetActive(true);
-                aimingHeadTransform.gameObject.SetActive(true);
-
-                notAimingBodyTransform.gameObject.SetActive(false);
-                notAimingHandTransform.gameObject.SetActive(false);
-            }
-            
-           
         }
- 
-        walkingChange = playerInput.GetAxis(InputManager.AXIS.MOVE);
     }
 
     private void FixedUpdate()
     {
         UpdateMovement();
         UpdateRotation();
+        UpdateAimingState();
     }
-    private void LateUpdate()
-    {
 
-        
-        /*if (animator.GetLayerWeight(1) == 1)
-        {
-            if(NeedBackwards)
-            {
-                mouseMaskTransform.position = new Vector3(mouseMaskTransform.position.x, mouseMaskTransform.position.y, 1f);
-            }
-            else if(!NeedBackwards)
-            {
-                mouseMaskTransform.position = new Vector3(mouseMaskTransform.position.x, mouseMaskTransform.position.y, -4.9f);
-            }
-
-
-        }
-        else
-        {
-            mouseMaskTransform.position = new Vector3(mouseMaskTransform.position.x, mouseMaskTransform.position.y,  -0.083f);
-
-        }*/
-    }
     private void UpdateMovement()
     {
         Vector3 movement;
@@ -174,10 +183,5 @@ public class PlayerController : MonoBehaviour
     {
        
         myRigidbody.MoveRotation(Quaternion.Euler(new Vector3(0, 90 * Mathf.Sign(targetTransform.position.x - transform.position.x), 0)));
-        
-
     }
-    
-
-
 }
