5a088dae909bc7a0f460baf1c28ddf877e3e865d
Shoffen <61907814+Shoffen@users.noreply.github.com>
2023-12-04 05:12:43 +0300

    Updating
    Made some preliminary version of shooting animation with responsive LEFT MOUSE CLICK that works only when player is aiming. It has some cooldown, so you can't spam it.

---
+++ Dragon Hunters/Assets/Scripts/InputManager.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public enum PLAYER_ACTION
+    {
+        INTERACTION,
+        AIMING,
+        SHOOTING
+        //SHOOT
+    }
+    public enum AXIS
+    {
+        MOVE
+    }
+    [SerializeField] private PlayerInput playerInput;
+    private InputActionMap actionMap;
+
+    private void Awake()
+    {
+        actionMap = playerInput.currentActionMap;
+        
+    }
+
+    private void OnEnable()
+    {
+        actionMap.Enable();
+    }
+
+    private void OnDisable()
+    {
+        actionMap.Disable();
+    }
+
+   
+    public Vector2 GetAxis(AXIS _axis)
+    {
+        
+        InputAction inputAction = actionMap[_axis.ToString().ToLower()];
+        switch(_axis)
+        {
+            case AXIS.MOVE:
+                Vector2 local = Vector2.zero;
+                local = new Vector2(inputAction.ReadValue<float>(), 0);
+                //Debug.Log(local);
+                return local;
+        }
+       // Debug.Log(inputAction.ReadValue<Vector2>());
+        return inputAction.ReadValue<Vector2>();
+       
+    }
+    public bool ListenForClick(PLAYER_ACTION _action = PLAYER_ACTION.INTERACTION)
+    {
+        if (actionMap != null)
+        {
+            string actionName = _action.ToString().ToLower();
+            InputAction inputAction = actionMap[actionName];
+
+            if (inputAction.triggered)
+            {
+                //Debug.Log("Action Pressed: " + _action);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
+
+    /*private void HandleAction(PLAYER_ACTION action)
+    {
+        Debug.Log("Performed action: " + action);
+        
+    }*/
+}
